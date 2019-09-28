2c08bb0886ae7003f4a3fed9625a8ae5ca53f693
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-09-28 10:43:13 +0530

    added feature to save leaderboards

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models.Player import Player
+import csv
 # Create your views here.
+
+
+def LeaderBoard(request):
+    if(request.GET.get('password') == "z12a34p"):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="leaderboards.csv"'
+
+        writer = csv.writer(response)
+        for player in Player.objects.all():
+            writer.writerow([player.name, player.email])
+        return response
+    else:
+        return HttpResponse("You are not authorized to see this page!")
